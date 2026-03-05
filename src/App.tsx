d92afd5367285db9d015eb1c4022a86be141ac07
Fuske <laminey2059@gmail.com>
2026-03-05 18:43:18 +0100

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Suspense, lazy } from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -65,6 +66,7 @@ const App = () => (
         <CookieBanner />
         <ScrollToTop />
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
